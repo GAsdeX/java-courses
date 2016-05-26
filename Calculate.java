8493fded100b28131ddb4f3b2d082bb4e82f3f6f
GAsdeX <grinya2102@gmail.com>
2016-05-26 13:14:58 +0300

    colculator

--- Calculate.java
+++ Calculate.java
@@ -1,1 +1,1 @@
-public class Calculate	{    public static void main(String[] args) {        System.out.println("Hooray");    }}
+public class Calculate	{    public static void main(String[] arg) {        System.out.println("Hooray");        int first = Integer.valueOf(arg[0]);        int second = Integer.valueOf(arg[1]);        int sum = first + second;        System.out.println("Sum:" + sum);    }}
